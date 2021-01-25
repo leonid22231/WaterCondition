450b0c7fd59983cb649fd60dad44f282a27bc344
leonid22231 <lsoldatov.lenya@gmail.com>
2021-01-25 19:07:23 +0700

    Add new file

---
+++ app/src/main/java/com/lyadev/watercondition/weight.java
@@ -0,0 +1,15 @@
+package com.lyadev.watercondition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+public class weight extends WearableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight);
+    }
+}
